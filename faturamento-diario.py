7e4295a7bd33dacc769881a375505861880dcb8b
jujudelu <106924180+jujudelu@users.noreply.github.com>
2023-04-02 11:17:42 -0300

    Add files via upload

---
+++ faturamento-diario.py
@@ -0,0 +1,22 @@
+faturamento = [100, 135, 320, 520, 420, 0, 0, 87, 230, 250, 308, 854, 0, 0]
+faturamento.sort()
+print ("O faturamento ordenado foi: ", faturamento)
+soma = sum([100, 135, 320, 520, 420, 0, 0, 87, 230, 250, 308, 854, 0, 0])
+media = sum([100, 135, 320, 520, 420, 87, 230, 250, 308, 854,])/10
+
+
+print("O faturamento total foi: ", soma)
+print ("O menor faturamento foi: ", faturamento[0])
+print ("O maior faturamento foi: ", faturamento[13])
+print ("A média de vendas foi: ", media)
+print ("Os valores maiores que a média foram: ", faturamento[13], faturamento[12], faturamento[11])
+
+
+
+
+
+
+
+
+
+
